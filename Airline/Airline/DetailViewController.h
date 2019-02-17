8a99aa7155f55b191ca911f7fdd549fe09caa585
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-17 14:44:54 +0700

    new files

---
+++ Airline/Airline/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Airline
+//
+//  Created by Thanaphat Suwannikornkul on 17/02/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Airline/Airline/DetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.m
+//  Airline
+//
+//  Created by Thanaphat Suwannikornkul on 17/02/62 BE.
+//  Copyright © 2562 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
